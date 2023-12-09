f738495ecfdde8f184ce4fe84fd1141d23b40e85
MessaoudiIshak <151960368+MessaoudiIshak@users.noreply.github.com>
2023-12-09 23:36:24 +0100

    popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,7 @@
-// Popup script
 function openPopup(title, description, imageUrl) {
-    // Create the popup container
     const popupContainer = document.createElement('div');
     popupContainer.className = 'popup-container';
 
-    // Create the popup content
     const content = document.createElement('div');
     content.className = 'popup-content';
 
@@ -12,8 +9,6 @@ function openPopup(title, description, imageUrl) {
     exitButton.innerText = 'X';
     exitButton.className = 'exit-button';
     exitButton.onclick = () => {
-        // Add action for the exit button
-        // For example, close the popup
         document.body.removeChild(popupContainer);
     };
 
@@ -32,8 +27,6 @@ function openPopup(title, description, imageUrl) {
     watchButton.innerText = 'Watch';
     watchButton.className = 'watch-button';
     watchButton.onclick = () => {
-        // Add action for the Watch button
-        // For example, redirect to a streaming service
     };
 
     content.appendChild(exitButton);
@@ -44,6 +37,7 @@ function openPopup(title, description, imageUrl) {
 
     popupContainer.appendChild(content);
 
-    // Append the popup container to the body
     document.body.appendChild(popupContainer);
+}
+
 }
